c1e5304023ffdabeb2b16b6133fce2f07f9e5f43
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-06-24 18:02:13 +0100

    adds project colours to tailwind config

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,3 +1,18 @@
 export default function Hero() {
-  return <section></section>;
+  return (
+    <section className="flex flex-col items-center px-[41px] py-[50px]">
+      <div className="">
+        <h2 className="font-bold">Creative</h2>
+        <h1 className="heading uppercase">
+          Full Stack <br /> Developer
+        </h1>
+        <div className="flex">
+          <p className="font-bold text-[16px]">React</p>
+          <p className="ml-[20px] font-bold text-[16px]">Next.js</p>
+          <p className="ml-[20px] font-bold text-[16px]">TypeScript</p>
+          <p className="ml-[20px] font-bold text-[16px]">GSAP</p>
+        </div>
+      </div>
+    </section>
+  );
 }
