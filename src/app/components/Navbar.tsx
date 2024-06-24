bdf69c71f153990a73ed91c68b67813c9b1f411e
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-06-24 17:52:29 +0100

    adds the navbar component to layout

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <header className="h-[100px]">
+      <nav className="flex items-center justify-between px-[20px] py-[40px]">
+        <h1 className="heading text-[16px] uppercase">Jason Blackburn</h1>
+        <button className="uppercase font-bold">Menu</button>
+      </nav>
+    </header>
+  );
+}
