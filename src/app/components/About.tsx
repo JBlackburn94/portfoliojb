67e59a9aa35b453296af58d54ac1c8c889799f0f
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-06-24 20:28:32 +0100

    adds the skills section

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,4 +1,43 @@
 export default function About() {
+  const mainSkills = [
+    {
+      id: 1,
+      name: "HTML",
+    },
+    {
+      id: 2,
+      name: "CSS",
+    },
+    {
+      id: 3,
+      name: "JavaScript",
+    },
+    {
+      id: 4,
+      name: "TypeScript",
+    },
+    {
+      id: 5,
+      name: "React",
+    },
+    {
+      id: 6,
+      name: "Next.js",
+    },
+    {
+      id: 7,
+      name: "GSAP",
+    },
+    {
+      id: 8,
+      name: "TailwindCSS",
+    },
+    {
+      id: 9,
+      name: "SQL",
+    },
+  ];
+
   return (
     <section className="about-bg h-screen mt-[61px]">
       <svg
@@ -10,7 +49,21 @@ export default function About() {
       >
         <path d="M430 0H0L319.723 41L430 0Z" fill="black" />
       </svg>
-      <div></div>
+      <div className="px-[41px]">
+        <h1 className="heading text-[48px] uppercase text-mainPink mb-[18px]">
+          Skills
+        </h1>
+        <div className="flex flex-wrap">
+          {mainSkills.map((mainSkill) => (
+            <p
+              className="bg-mainPink mb-[42px] px-[12px] py-[10px] rounded-md mr-[14px] font-bold"
+              key={mainSkill.id}
+            >
+              {mainSkill.name}
+            </p>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
