9ebb84cb4194475faa3ec200ccd5ae3055cebee3
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-06-24 22:21:13 +0100

    adds animation to the hero section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,6 +3,11 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import hero from "../../../public/computer.svg";
 import desktopHero from "../../../public/desktopComputer.svg";
+import { PiHandWavingFill } from "react-icons/pi";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 
 export default function Hero() {
   const [width, setWidth] = useState<number>(0);
@@ -19,19 +24,47 @@ export default function Hero() {
     };
   });
 
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    tl.to("#image-container", {
+      opacity: 1,
+      duration: 0.4,
+    });
+    gsap.to("#waving", {
+      delay: 0.5,
+      duration: 0.4,
+      rotation: 40,
+      repeat: -1,
+      yoyo: true,
+    });
+    gsap.to("#content-container", {
+      delay: 0.5,
+      opacity: 1,
+      duration: 0.4,
+      stagger: 1,
+    });
+  });
+
   return (
     <section className="flex flex-col md:flex-row-reverse justify-center md:justify-between items-center px-[41px] py-[50px] lg:px-[185px]">
-      <div className="flex flex-col items-center justify-center md:ml-[100px] lg:ml-[166px]">
-        <h2 className="top-[140px] md:top-[110px] right-[20px] lg:right-[30px] lg:top-[150px] relative rotate-3 font-bold text-[20px] lg:text-[24px]">
-          I am <span className="text-mainPink">Jason</span>
-        </h2>
+      <div
+        className="flex flex-col items-center justify-center relative md:ml-[100px] lg:ml-[166px] opacity-0"
+        id="image-container"
+      >
+        <PiHandWavingFill
+          className="text-mainPink text-[70px] md:text-[50px] lg:text-[80px] absolute m-auto left-0 right-12 bottom-16 md:bottom-14 lg:bottom-16 top-0"
+          id="waving"
+        />
         <Image
           src={width > 390 ? desktopHero : hero}
           alt="A vector image of a computer monitor"
           priority
         />
       </div>
-      <div className="flex flex-col mt-[28px] md:mt-0">
+      <div
+        className="flex flex-col mt-[28px] md:mt-0 opacity-0"
+        id="content-container"
+      >
         <h2 className="font-bold text-mainPink text-[28px] lg:text-[40px] leading-none ">
           Creative
         </h2>
