b895d1f4830d310822dab1614a9e6917c2ad5fd1
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-06-25 16:17:09 +0100

    adds a background blur to the navbar and a fixed position

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <header className="h-[100px]">
-      <nav className="flex items-center justify-between px-[20px] py-[40px]">
+      <nav className="flex items-center justify-between px-[20px] py-[40px] fixed w-full z-10 backdrop-blur-sm">
         <h1 className="heading text-[16px] uppercase">Jason Blackburn</h1>
         <button className="uppercase font-bold">Menu</button>
       </nav>
