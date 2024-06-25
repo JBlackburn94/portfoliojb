97aa6bdf3ee11a24e02a168bacdf2fdd2b4c15b8
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2024-06-25 19:30:54 +0100

    adds nav animation so that the logo opacity goes to 0 after a certain scroll point

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,30 @@
+"use client";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Navbar() {
+  useGSAP(() => {
+    gsap.to("#logo", {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: "#logo",
+        start: "top 5%",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+  });
+
   return (
-    <header className="h-[100px] backdrop-blur-sm fixed w-full z-10">
+    <header className="h-[100px] fixed w-full z-10">
       <nav className="flex items-center justify-between px-[20px] py-[40px]">
-        <h1 className="heading text-[16px] uppercase">Jason Blackburn</h1>
+        <h1 className="heading text-[16px] uppercase" id="logo">
+          Jason Blackburn
+        </h1>
         <button className="uppercase font-bold">Menu</button>
       </nav>
     </header>
